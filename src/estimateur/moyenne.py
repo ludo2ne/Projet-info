8de44cedfbc003ed14e09c1d93b0cd453cf6e5ba
ludo2ne <92184287+ludo2ne@users.noreply.github.com>
2022-05-04 23:47:36 +0200

    Add files via upload

---
+++ src/estimateur/moyenne.py
@@ -0,0 +1,63 @@
+'''
+Module moyenne
+Auteurs : Deneuville Ludovic, Trotta Jean-Philippe et Villacampa Laurene
+Date    : 05/05/2022
+Licence : Domaine public
+Version : 1.0
+'''
+import doctest
+
+
+class Moyenne:
+    '''Moyenne d'une variable
+
+    Cette classe ne contient qu'une seule méthode statique
+    Il n'y a pas de constructeur car il n'est pas nécessaire d'instancier une objet de cette classe
+    '''
+
+    @staticmethod
+    def appliquer(table, numero_colonne):
+        '''Calculer la moyenne d'une colonne de la table
+
+        Parameters
+        ----------
+        table : TableDonnees
+            table de données
+        numero_colonne : int
+            index de la colonne
+
+        Returns
+        -------
+            float : moyenne des valeurs de la colonne
+
+        Examples
+        --------
+
+        '''
+
+        print("------------------------------------------------------")
+        print("Calcul de la moyenne de la variable " +
+              table.donnees[0][numero_colonne])
+
+        somme = 0
+        nb = 0
+        nb_na = 0
+
+        for i in range(1, len(table.donnees)):
+            if table.donnees[i][numero_colonne] != "mq":
+                somme += float(table.donnees[i][numero_colonne])
+                nb += 1
+            else:
+                nb_na += 1
+
+        moyenne = round(somme / nb, 2)
+
+        print("  Moyenne : " + str(round(moyenne, 2)) +
+              " (sur " + str(nb) + " valeurs renseignées et " + str(nb_na) + " valeurs manquantes)")
+
+        return moyenne
+
+    def __str__(self):
+        '''Conversion de l'objet en chaîne de caractères
+        '''
+        return "Je suis une moyenne"
