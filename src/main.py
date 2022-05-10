5f303109849aa6fffc329ab7c10d75af61f3aa64
Ludovic Deneuville <ludovic.deneuville@eleve.ensai.fr>
2022-05-10 10:12:18 +0200

    reorder data

--- src/main.py
+++ src/main.py
@@ -8,23 +8,21 @@ Version : 1.0
 from table.tabledonnees import TableDonnees
 from pipeline.pipeline import Pipeline
 from transformation.centrage import Centrage
-from transformation.selectionVariables import SelectionVariables
+from transformation.selectionvariables import SelectionVariables
 
 
 # tests des fonctionnalités
 
 ma_table = TableDonnees(nom="table_test",
-                        chemin_complet="P:/projet-info-sources/Projet-info/src/tests/donnees/synop.201301.csv.gz")
+                        chemin_complet="P:/projet-info-sources/Projet-info/donnees/synop.201301.csv.gz")
 
 ma_table.afficher(nb_lignes=5,
                   nb_colonnes=15)
 
-
-mon_centrage = Centrage(['ff', 't', 'xxxxx'])
-ma_selection = SelectionVariables(['numer_sta', 'ff'])
-
+# Creation du pipeline
 mon_premier_pipeline = Pipeline(nom="pipo",
-                                liste_transformations=[mon_centrage, ma_selection])
+                                liste_transformations=[Centrage(['ff', 't', 'xxxxx']),
+                                                       SelectionVariables(['numer_sta', 'ff'])])
 mon_premier_pipeline.lancer(ma_table)
 
 ma_table.afficher(nb_lignes=5,
