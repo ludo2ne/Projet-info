d10bda151ee80f27d40a7fd734d1b4b140c0eea9
Jean-Philippe TROTTA <id1964@ensai.fr>
2022-05-16 15:15:33 +0200

    we

---
+++ src/lien2var/lien2var.py
@@ -0,0 +1,16 @@
+'''
+Module lien2var
+Auteurs : Deneuville Ludovic, Trotta Jean-Philippe et Villacampa Laurene
+Date    : 13/05/2022
+Licence : Domaine public
+Version : 1.0
+'''
+
+import doctest
+
+class Lien2var:
+    '''Classe (abstraite ?) pour étudier les liens entre deux variables d'une table donnée'''
+
+    def __init__(self):
+        '''Constructeur de l'objet'''
+        pass
---
+++ src/lien2var/testchi2.py
@@ -0,0 +1,16 @@
+'''
+Module testchi2
+Auteurs : Deneuville Ludovic, Trotta Jean-Philippe et Villacampa Laurene
+Date    : 13/05/2022
+Licence : Domaine public
+Version : 1.0
+'''
+
+import doctest
+
+class Testchi2(Lien2var):
+    '''Test d'indépendance du X² de deux variables numériques d'une table de données'''
+
+    def __init__(self):
+        '''Constructeur de l'objet'''
+        pass
