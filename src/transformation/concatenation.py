ce93efae1cd1b3780244954261a22db48bfaf49d
Laurène Villacampa <laurene.villacampa@eleve.ensai.fr>
2022-05-16 21:12:34 +0200

    concatenate

--- src/transformation/concatenation.py
+++ src/transformation/concatenation.py
@@ -38,14 +38,24 @@ class ConcatanationLignes(Transformation):
         #assert(table.type_var == self.autre_table.type_var)
 
         if np.array_equal(table.variables, self.autre_table.variables):
-            donnees_conc = np.concatenate((
-                table.donnees, self.autre_table.donnees))
+            # liste vide avec "la bonne taille"
+            donnees = [[] for k in range(
+                len(table.donnees) + len(self.autre_table.donnees) + 1)]
+            # 1er element : nom des variables
+            donnees[0] = np.asarray(table.variables)
+            donnees_conc = np.concatenate(
+                (table.donnees, self.autre_table.donnees))  # concaténation des données
+            for k in range(len(donnees_conc)):
+                donnees[k+1] = donnees_conc[k]  # ajout des lignes
+
+            #donnees = np.asarray(table.variables)
+            #table.donnees = np.concatenate((table.donnees, self.autre_table.donnees))
 
             print('Les tables ' + table.nom + ' et ' +
                   self.autre_table.nom + ' ont été concaténées')
 
             return TableDonnees(table.nom + '_' + self.autre_table.nom,
-                                donnees_conc,
+                                donnees=np.asarray(donnees),
                                 identifiants=None,
                                 type_var=table.type_var,
                                 valeur_manquante="na")
