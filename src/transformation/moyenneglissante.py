faa48ab9026bd3acf0de2f8a1593e5d099125fd8
Ludovic Deneuville <ludovic.deneuville@eleve.ensai.fr>
2022-05-17 00:01:00 +0200

    fix moyenneglissante wip

--- src/transformation/moyenneglissante.py
+++ src/transformation/moyenneglissante.py
@@ -26,24 +26,21 @@ class MoyenneGlissante(Transformation):
         pass
 # est-ce que la liste_colonnes est vraiment un attribut la classe de MoyenneGlissante ? si oui l'ajouter sur l'UML, sinon le supprimer de la documentation TODO
 
-    @staticmethod
-    def liste_colonne(table, numero_colonne):
+    def liste_colonne(self, table, numero_colonne):
         '''converti les données d'une colonne de la table en une liste'''
         liste = []
         for i in range(len(table.donnees)):
             liste.append(table.donnees[i][numero_colonne])
-        return liste #cette méthode sera utile pour la classe Lien2Var : est-ce qu'il vaut mieux la  mettre en méthode de TableDonnées ? #TODO
+        return liste  # cette méthode sera utile pour la classe Lien2Var : est-ce qu'il vaut mieux la  mettre en méthode de TableDonnées ? #TODO
 
-    @staticmethod
-    def sous_liste(liste,debut,fin):
+    def sous_liste(self, liste, debut, fin):
         '''extrait une sous liste'''
         ss_list = []
-        for i in range(debut,fin+1):
+        for i in range(debut, fin+1):
             ss_list.append(liste[i])
         return ss_list
 
-    @staticmethod
-    def moyenne_glissante(table, numero_colonne, pas):
+    def moyenne_glissante(self, table, numero_colonne, pas):
         '''Calculer la liste des moyennes glissantes à d'une variable de la table
 
         Parameters
@@ -53,35 +50,39 @@ class MoyenneGlissante(Transformation):
         numero_colonne : int
             numéro de la colonne sur laquelle appliquer
         '''
-        liste_valeurs = liste_colonne(table,numero_colonne)
+        liste_valeurs = self.liste_colonne(table, numero_colonne)
         liste_moyennes = []
 
-        if pas%2==1:  #cas impair
-            for i in range((pas-1)/2):
-                liste_moyennes.append("na") #pour les premières valeurs
-            for i in range((pas-1)/2,len(table.donnees)-(pas-1)/2+1):
-                ss_liste=sous_liste(liste_valeurs,i-(pas-1)/2,i+(pas-1)/2)
-                if not np.isnan(ss_liste):
-                    moyenne=statistics.mean(ss_liste)
-                else:
-                    moyenne="na"
-                liste_moyennes.append(moyenne)
-            for i in range((pas-1)/2):
-                liste_moyennes.append("na") #pour les dernières valeurs
+        if pas % 2 == 1:  # cas impair
 
-        if pas%2==0: #cas pair
+            demi_pas = int((pas-1) / 2)
+
+            for i in range(demi_pas):
+                liste_moyennes.append("na")  # pour les premières valeurs
+            for i in range(demi_pas, len(table.donnees)-demi_pas+1):
+                ss_liste = self.sous_liste(
+                    liste_valeurs, i-demi_pas, i+demi_pas)
+                if not np.isnan(ss_liste):
+                    moyenne = statistics.mean(ss_liste)
+                else:
+                    moyenne = "na"
+                liste_moyennes.append(moyenne)
+            for i in range(demi_pas):
+                liste_moyennes.append("na")  # pour les dernières valeurs
+
+        if pas % 2 == 0:  # cas pair
             for i in range(pas/2):
-                liste_moyennes.append("na") #pour les premières valeurs
-            for i in range(pas/2,len(table.donnees)-pas/2+1):
-                ss_liste=sous_liste(liste_valeurs,i-pas/2,i+pas/2)
+                liste_moyennes.append("na")  # pour les premières valeurs
+            for i in range(pas/2, len(table.donnees)-pas/2+1):
+                ss_liste = self.sous_liste(liste_valeurs, i-pas/2, i+pas/2)
                 ss_liste.pop(i)
                 if not np.isnan(ss_liste):
-                    moyenne=statistics.mean(ss_liste)
+                    moyenne = statistics.mean(ss_liste)
                 else:
-                    moyenne="na"
+                    moyenne = "na"
                 liste_moyennes.append(moyenne)
             for i in range(pas/2):
-                liste_moyennes.append("na") #pour les dernières valeurs
+                liste_moyennes.append("na")  # pour les dernières valeurs
 
         return liste_moyennes
 
@@ -95,10 +96,9 @@ class MoyenneGlissante(Transformation):
         numero_colonne : int
             numéro de la colonne sur laquelle appliquer
         '''
-        liste_moyennes = moyenne_glissante(table, numero_colonne, pas)
+        liste_moyennes = self.moyenne_glissante(table, numero_colonne, pas)
         for i in range(len(table.donnees)):
             table.donnees[i][numero_colonne] = liste_moyennes[i]
-
 
     def appliquer(self, table):
         '''Appliquer la transformation à toutes les variables numériques de la table
