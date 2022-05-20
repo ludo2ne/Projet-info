a9d6ef17a4f1c01093fc59ee33001d690116adcb
Laurène Villacampa <laurene.villacampa@eleve.ensai.fr>
2022-05-20 23:17:03 +0200

    pipeline MaJ

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -9,23 +9,23 @@ from transformation.export import Export
 
 
 class Pipeline:
-    '''Classe représentant un pipeline
+    '''Classe implémentant un pipeline, liste d'opérations à réaliser. 
 
     Attributes
     ----------
     nom : str
         Nom du pipeline
-    liste_transformations : list[Transformation]
-        liste des transformations qui seront appliquées
-    estimateur : Estimateur
-        estimateur final à appliquer
+    liste_operations : list[Transformation, Estimateur, LienVar]
+        liste des opérations qui seront appliquées
     '''
-    #proposition : remplacer l'attribut liste_transformation par liste_operation (où la dernière? opération est un estimateur ou lienvar)
-    #si estimateur et lienvar ne modifie pas la table initiale, inutile qu'ils soient en dernier d'ailleurs
-    # supprimer l'attibut estimateur en conséquent, idemem pour les paramètres de init
-    # supprimer aussi le paramètre exporter_table TODO
+    # DONE    proposition : remplacer l'attribut liste_transformation par liste_operation (où la dernière? opération est un estimateur ou lienvar)
+    # OK      si estimateur et lienvar ne modifie pas la table initiale, inutile qu'ils soient en dernier d'ailleurs
+    # DONE    supprimer l'attibut estimateur en conséquent, idemem pour les paramètres de init
+    # DONE    supprimer aussi le paramètre exporter_table
     # question : et si on veut exporter la table du return de estimateur (pas celle sur laquelle le pipeline est appliqué), que faire ?
-    def __init__(self, nom, liste_transformations, estimateur=None, exporter_table=False):
+    # TODO cf UML : méthodes ajouter_oper et supprimer_oper ? (pas sûre que ce soit nécessaire ? -> manipulation de listes ?)
+
+    def __init__(self, nom, liste_transformations):
         '''Constructeur de l'objet
 
         Parameters
@@ -34,39 +34,33 @@ class Pipeline:
             Nom du pipeline
         liste_transformations : list[Transformation]
             liste des transformations qui seront appliquées
-        estimateur: Estimateur
-            estimateur final à appliquer
-            None par défaut
         exporter_table : bool
             Si True le fichier est exporté dans donnees/export
             False par défaut
         '''
         self.nom = nom
         self.liste_transformations = liste_transformations
-        self.estimateur = estimateur
-        self.exporter_table = exporter_table
 
     def lancer(self, table):
         '''Lancement du pipeline
 
-        La liste des transformations est appliquée dans l'ordre à la table en paramètre
+        La liste des opérations est appliquée dans l'ordre à la table en paramètre
 
         Parameters
         ----------
         table : tableDonnees
             table de données à transformer
 
-        Returns
+        Returns 
         -------
         table
             table de données mise à jour
         '''
+        # TODO Question : y a-t-il vraiment un "Returns" ?
+
         print("------------------------------------------------------")
         print("Lancement du pipeline " + self.nom)
 
         # Pour chaque transformation de la liste
         for i in range(len(self.liste_transformations)):
             self.liste_transformations[i].appliquer(table)
-
-        if self.exporter_table:
-            Export.appliquer(table)
